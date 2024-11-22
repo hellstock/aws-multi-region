0f2f671834db10044cdd2675c0f3f7ed1b422b9b
Jani Helin <jani.helin@gmail.com>
2024-11-22 18:49:17 +0200

    Lambda response contains region

--- backend/lambda/lambda_hello.py
+++ backend/lambda/lambda_hello.py
@@ -1,5 +1,12 @@
+import os
+
 def handler(event, context):
+    region = os.environ.get('AWS_REGION', 'Unknown Region')
+
+    response_string = f"Hello from Hush Lambda in {region}!"
+    print(f"Returning to client: {response_string}")
+
     return {
         "statusCode": 200,
-        "body": "Hello from Hush Lambda!"
+        "body": response_string
     }
