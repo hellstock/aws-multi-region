71724ac92b175611cee2c0d5b7f1d27d7729e6f9
Jani Helin <jani.helin@gmail.com>
2024-11-22 14:01:22 +0200

    Script improvement

--- infrastructure/assume-role.sh
+++ infrastructure/assume-role.sh
@@ -10,9 +10,13 @@ SESSION_NAME="CDKRoleSession"
 
 CREDS=$(aws sts assume-role --role-arn $ROLE_ARN --role-session-name $SESSION_NAME)
 
+if [ $? -ne 0 ] || [ -z "$CREDS" ]; then
+  echo "Error: Failed to assume role. Check AWS CLI configuration and permissions."
+  exit 1
+fi
+
 export AWS_ACCESS_KEY_ID=$(echo $CREDS | jq -r '.Credentials.AccessKeyId')
 export AWS_SECRET_ACCESS_KEY=$(echo $CREDS | jq -r '.Credentials.SecretAccessKey')
 export AWS_SESSION_TOKEN=$(echo $CREDS | jq -r '.Credentials.SessionToken')
 
 echo "Temporary credentials set for AWS CLI/SDK use."
-
