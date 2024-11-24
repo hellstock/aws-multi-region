e01df1d363c3bfd7f8eff0ad7b3a0213c3cbf718
Jani Helin <jani.helin@gmail.com>
2024-11-24 15:25:33 +0200

    More test cases

--- tests/rest-api/api-test.py
+++ tests/rest-api/api-test.py
@@ -23,7 +23,6 @@ class TestApiEndpoint(unittest.TestCase):
         expected_substring = "Hush"
         self.assertIn(expected_substring, response.text, f"'{expected_substring}' not found in response body")
 
-
     def test_get_hello_region(self):
         endpoint = f"{self.base_url}/hello"
         response = requests.get(endpoint)
@@ -32,6 +31,19 @@ class TestApiEndpoint(unittest.TestCase):
 
         expected_region = self.aws_reqion
         self.assertIn(expected_region, response.text, f"'{expected_region}' not found in response body")
+
+    def test_get_hello_missing_authentication(self):
+        endpoint = f"{self.base_url}/helloauthenticated"
+        response = requests.get(endpoint)
+
+        self.assertEqual(response.status_code, 403, f"Expected 403 but got {response.status_code}")
+
+    def test_missing_endpoint(self):
+        endpoint = f"{self.base_url}/notrealendpoint"
+        response = requests.get(endpoint)
+
+        # ApiGw returns 403
+        self.assertEqual(response.status_code, 403, f"Expected 403 but got {response.status_code}")
 
 if __name__ == "__main__":
     unittest.main()
