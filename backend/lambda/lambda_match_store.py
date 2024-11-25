bd5da456a1cb1823d2d130c716428898cfd0da0e
Jani Helin <jani.helin@gmail.com>
2024-11-25 22:14:14 +0200

    Fix match storing lambda

--- backend/lambda/lambda_match_store.py
+++ backend/lambda/lambda_match_store.py
@@ -1,5 +1,6 @@
 import boto3
 import os
+import json
 from datetime import datetime
 
 dynamodb = boto3.resource("dynamodb")
@@ -7,11 +8,22 @@ table_name = os.environ["DYNAMO_TABLE_NAME"]
 table = dynamodb.Table(table_name)
 
 def handler(event, context):
-    tournament_id = event["tournament_id"]
-    match_id = event["match_id"]
-    player1 = event["player1"]
-    player2 = event["player2"]
-    score = event["score"]
+
+    print(f'Incoming event: {event}')
+
+    try:
+        body = json.loads(event["body"])  # Extract and parse the request body
+    except (KeyError, json.JSONDecodeError) as e:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Invalid request body"})
+        }
+
+    tournament_id = body.get("tournament_id")
+    match_id = body.get("match_id")
+    player1 = body.get("player1")
+    player2 = body.get("player2")
+    score = body.get("score")
 
     item = {
         "PK": f"TOURNAMENT#{tournament_id}",
@@ -21,6 +33,8 @@ def handler(event, context):
         "Score": score,
         "Timestamp": datetime.utcnow().isoformat(),
     }
+
+    print(f'Storing to Dynamo: {item}')
 
     table.put_item(Item=item)
 
