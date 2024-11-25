845b9c5dd40f6c88bdb6357f5903fd26a0031f4f
Jani Helin <jani.helin@gmail.com>
2024-11-25 09:24:12 +0200

    Initial DynamoDB table

---
+++ infrastructure/hush_dynamo_stack.py
@@ -0,0 +1,42 @@
+from aws_cdk import (
+    Stack,
+    aws_dynamodb as dynamodb,
+    RemovalPolicy,
+)
+from constructs import Construct
+
+
+class HushDynamoDbStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Create DynamoDB for sigle table design and on-demand provisioning
+        self.table = dynamodb.Table(
+            self,
+            "HushSingleTable",
+            partition_key=dynamodb.Attribute(
+                name="PK",
+                type=dynamodb.AttributeType.STRING,
+            ),
+            sort_key=dynamodb.Attribute(
+                name="SK",
+                type=dynamodb.AttributeType.STRING,
+            ),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        # Add a GSI for performance optimization on additional
+        # access patterns
+        self.table.add_global_secondary_index(
+            index_name="GSI1",
+            partition_key=dynamodb.Attribute(
+                name="GSI1PK",
+                type=dynamodb.AttributeType.STRING,
+            ),
+            sort_key=dynamodb.Attribute(
+                name="GSI1SK",
+                type=dynamodb.AttributeType.STRING,
+            ),
+            projection_type=dynamodb.ProjectionType.ALL,
+        )
